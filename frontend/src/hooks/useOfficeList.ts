fffee8c9ee97195a246f6f6e3348cc8d941e3e1a
Freddy <freddypalencia125@gmail.com>
2024-12-18 15:51:16 -0300

    refactor: increase timeout duration in useOfficeList hook from 30s to 60s

--- frontend/src/hooks/useOfficeList.ts
+++ frontend/src/hooks/useOfficeList.ts
@@ -4,7 +4,7 @@ import { Offices } from '../types';
 
 const useOfficeList = () => {
   // Set Time
-  const time = 30000;
+  const time = 60000;
 
   // Create State
   const [offices, setOffices] = useState<Offices[]>([]);
